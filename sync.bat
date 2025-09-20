25ffc16282c4da0fd06ded8bdfbdc60e57837a1b
Astralis0 <aurora.spark17@gmail.com>
2025-09-20 01:14:10 +0100

    commit

--- sync.bat
+++ sync.bat
@@ -1,4 +1,10 @@
 cd ../drama-portfolio-website
+scp temp/drama-portfolio-website .
+
+cd drama-portfolio-website
+del .\index.html
+ren .\index-content.html index.html
+
 git add --all
 git commit -a -m commit
 git push
